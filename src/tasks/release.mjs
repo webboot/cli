d7e384687b1cd644dcab38eacd5ebb51ad53c507
jascha <jascha@jaeh.at>
2020-02-14 07:43:48 +0100

    tasks/release: nicer output

--- src/tasks/release.mjs
+++ src/tasks/release.mjs
@@ -10,32 +10,21 @@ export const release = async signed => {
 
   log.success('\n@webboot has collected all needed data.\n')
 
-  // await Promise.all(
-  // Object.entries(signed).map(async ([key, val]) => {
-  // if (key === 'hashes') {
-  // log.success(key, JSON.stringify(JSON.parse(val), null, 2))
-  // return
-  // }
+  log.warn('DATA WILL BE PUBLISHED', 'the following data will be sent to https://api.webboot.org')
 
-  // if (key === 'sig') {
-  // log.success('sig', 'signed and encrypted sri hashes')
-  // return
-  // }
+  log.annotate('-------------------------------------------------------\n')
 
-  // if (key === 'key') {
-  // log.success('key', 'your pgp public key')
-  // return
-  // }
+  // print the signed object
+  Object.entries(signed.signed).map(([key, value]) => {
+    log.warn(key, typeof value === 'string' ? value.substr(0, 30) : value)
+  })
 
-  // log.success(key, val.split('\n')[0])
-  // }),
-  // )
+  log.annotate('-------------------------------------------------------\n')
 
-  log('the data above will be sent to https://api.webboot.org')
   log(`@webboot will verify the hashes using ${signed.domain} and then publish this data.`)
   log.warn('this can not be undone.')
 
-  const wantsToSend = await cli.prompt('Do you want to publish this data?', { yesNo: true })
+  const wantsToSend = await cli.prompt('Do you want to publish this data? (y/N) : ', { yesNo: true })
 
   if (wantsToSend) {
     const body = JSON.stringify(signed)
@@ -50,7 +39,7 @@ export const release = async signed => {
 
     const response = await webboot.httpRequest('http://localhost:8080/v1/release/', options)
 
-    console.log({ response })
+    console.log('api response:', response)
   }
 
   log.error('E_NOT_IMPLEMENTED', `${libName} not implemented yet.`)
